c42e2d2d1589298459244734c7f9f8e4ec1e1324
Richard <programmingislife23@gmail.com>
2024-11-12 16:51:14 -0300

    Criando homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage/AuthForm"
 import { AuthPage } from "./pages/HomePage/AuthPage/AuthPage"
+import  Home  from "./pages/HomePage/HomePage/HomePage"
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element ={<HomePage />} />
+        <Route path='/' element ={<Home />} />
         <Route path='/auth' element ={<AuthPage />} />
       </Routes>
     </>
---
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -0,0 +1,12 @@
+import { Flex } from '@chakra-ui/react'
+
+const PageLayout = () => {
+    return (
+        <Flex>
+            {/* barra lateral na esquerda */}
+            {/* conteudo da página na direita */}
+        </Flex>
+    )
+}
+
+export default PageLayout
